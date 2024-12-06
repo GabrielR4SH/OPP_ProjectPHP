27d3b85e0c10b0ee275bfb1947b3c5b60204331d
GabrielR4SH <bieelsantana23@hotmail.com>
2024-12-06 20:59:30 -0300

    first commit

---
+++ debug/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Desktop\Opp\ContaBancaria;
+
+$contaBancaria = new ContaBancaria();
+var_dump($contaBancaria);
---
+++ src/ContaBancaria.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Desktop\Opp;
+
+class ContaBancaria {
+}
